55e672eaf31fcbd5bcd4552ff1777240aa9bad94
Muhammad Malik Musthofa <muhammadmalikmusthofa@gmail.com>
2023-02-09 18:54:13 +0700

    semangat

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,73 @@
 const { Class, User, Course, Profile } = require('../models');
 
 class UserController {
+
+    static home(req,res){
+        res.redirect('/login')
+    }
     static login(req, res) {
+        res.render('login')
+    }
+    static registerStudent(req, res) {
+        res.render('registrasiStudent')
+    }
+    static registerTeacher(req, res) {
+        res.render('registrasiTeacher')
+    }
+
+    static createUser(req,res){
+        let {
+            firstName,
+            lastName,
+            username,
+            password,
+            email,
+            dateOfBirth,
+            city,
+            school,
+            degree,
+            university,
+            role
+        } = req.body
+         User.create({username,password,email,role})
+        
+        .then(user=>{
+            Profile.create({firstName,lastName,dateOfBirth,school,city,degree,university, UserId: user.id})
+        })
+        .then(()=> res.redirect('login'))
+        .catch(e=>res.send(e))
+    }
+
+    static viewTeacher(req,res){
+        const {id} = req.params
         User.findAll({
-            include: ['lectures']
-        }).then(result => {
-            res.send(result)
-        }).catch(err => {
-            res.send(err)
-        });
-        // res.render('login')
+            include: [Profile,'lectures'],
+            where:{
+                id,
+                'role': 'teacher',
+            }
+        })
+        .then(teacher=> {  
+            if(teacher.length === 0) throw ('teacher not found');
+            res.render('teacherDetail',{teacher})
+        })
+        .catch(e=>res.send(e))
     }
-    static register(req, res) {
-        res.render('registrasi')
+
+    static addCourse(req,res){
+        let {id} = req.params
+        User.findByPk(id)
+        .then(user => res.render('addCourse', {user}))
+        .catch(e=>res.send(e))
     }
+    static createCourse(req,res){
+        const {id} = req.params;
+        const {name,description,image,duration,category} = req.body
+        Course.create({name,description,image,duration,category,TeacherId: id})
+        .then(()=> res.redirect(`/teacher/${id}`))
+        .catch(e=>res.send(e))
+    }
+  
 }
 
 module.exports = UserController
--- router/index.js
+++ router/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const router = express.Router()
 const userController = require('../controllers/userController')
 
+router.get('/', userController.home)
 router.get('/login', userController.login)
-router.get('/register', userController.register)
+router.get('/register/student', userController.registerStudent)
+router.get('/register/teacher', userController.registerTeacher)
+router.post('/register', userController.createUser)
+router.get('/teacher/:id',userController.viewTeacher)
+router.get('/teacher/:id/add', userController.addCourse)
+router.post('/teacher/:id/add', userController.createCourse)
+
 
 module.exports= router;
